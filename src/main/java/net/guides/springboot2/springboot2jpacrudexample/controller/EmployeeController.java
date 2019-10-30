aecf9e0378ac87924b12acbd4c80b6340f9799ca
Piotr <piotrekchorzow@interia.eu>
2019-10-30 22:33:53 +0100

    Initial commit

---
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package net.guides.springboot2.springboot2jpacrudexample.controller;
+
+import lombok.AllArgsConstructor;
+import net.guides.springboot2.springboot2jpacrudexample.model.Employee;
+import net.guides.springboot2.springboot2jpacrudexample.repository.EmployeeRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/v1")
+@AllArgsConstructor
+public class EmployeeController {
+    EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById
+
+
+
+}
---
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/model/Employee.java
@@ -0,0 +1,23 @@
+package net.guides.springboot2.springboot2jpacrudexample.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String emailId;
+
+}
